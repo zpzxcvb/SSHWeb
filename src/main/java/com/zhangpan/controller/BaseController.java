63e86c09753610e670fd492db8172c23a2f5e50e
zpzxcvb <527517062@qq.com>
2017-03-22 15:56:42 +0800

    11

---
+++ src/main/java/com/zhangpan/controller/BaseController.java
@@ -0,0 +1,28 @@
+package com.zhangpan.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController {
+	protected HttpServletRequest request;
+	protected HttpServletResponse response;
+	protected HttpSession session;
+
+	@ModelAttribute
+	public void setReqAndRes(HttpServletRequest request, HttpServletResponse response){
+       this.request = request;
+       this.response = response;
+       this.session = request.getSession();
+    }
+	
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+}
