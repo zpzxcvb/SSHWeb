ac16c47d35d5a69d7ad18eaef32bf96b27a9a04c
zpzxcvb <527517062@qq.com>
2017-04-05 17:09:29 +0800

    提交本地

--- src/test/java/com/zhangpan/ssh/test/Test.java
+++ src/test/java/com/zhangpan/ssh/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		Test t=new Test(); 
-		String reg="a<html>123456</html>c";
+		String reg="a<html>123456</html> c";
 		System.out.println(reg.matches("^a(.*)c$"));
 	}
 }
