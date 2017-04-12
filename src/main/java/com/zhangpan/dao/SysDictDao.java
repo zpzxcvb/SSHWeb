b1d4475e2da2eff8150e4de0810a312e59dd53e6
zpzxcvb <527517062@qq.com>
2017-04-12 11:24:27 +0800

    1

--- src/main/java/com/zhangpan/dao/SysDictDao.java
+++ src/main/java/com/zhangpan/dao/SysDictDao.java
@@ -1,7 +1,5 @@
 package com.zhangpan.dao;
 
-import org.springframework.stereotype.Repository;
-
 import com.zhangpan.model.SysDictType;
 
 public interface SysDictDao extends BaseDao<SysDictType,Integer> {
--- src/main/java/com/zhangpan/dao/SysUserDao.java
+++ src/main/java/com/zhangpan/dao/SysUserDao.java
@@ -1,7 +1,5 @@
 package com.zhangpan.dao;
 
-import org.springframework.stereotype.Repository;
-
 import com.zhangpan.model.SysUser;
 
 public interface SysUserDao extends BaseDao<SysUser,Integer> {
